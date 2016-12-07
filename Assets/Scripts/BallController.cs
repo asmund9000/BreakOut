1941574ad7f69e93282b593a1f2406e8267194ee
asm <xasmundx@gmail.com>
2016-12-07 19:52:56 +0700

    Контроллер платформы, шарика, рассчёт отскока мячика.

---
+++ Assets/Scripts/BallController.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BallController : MonoBehaviour {
+
+
+    private float _ballSpeed;
+    private Vector2 _moveDirection;
+    private Transform _myTransform;
+
+	// Use this for initialization
+	void Start () {
+        _myTransform = transform;
+        _ballSpeed = 7;
+        _moveDirection = Vector3.up;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        transform.Translate(_moveDirection * Time.deltaTime * _ballSpeed);
+        
+	}
+
+    void OnCollisionEnter2D(Collision2D collision)
+    {
+       // Debug.Log("Collision");
+        ContactPoint2D contact = collision.contacts[0];
+        _moveDirection = Vector3.Reflect(_moveDirection, contact.normal);
+    }
+
+
+}
---
+++ Assets/Scripts/PlatformController.cs
@@ -0,0 +1,59 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlatformController : MonoBehaviour {
+
+    private float _platformSpeed;
+    private Transform _myTransform;
+    private Vector2 _screenPoint;
+    private float _platformBoundSize;
+
+    // Use this for initialization
+    void Start() {
+        _myTransform = transform;
+        _platformBoundSize = GetComponent<SpriteRenderer>().bounds.size.x;
+        _platformSpeed = 4;
+    }
+
+    // Update is called once per frame
+    void Update() {
+      //  float pos = _myTransform.position.x + (Input.GetAxis("Horizontal") * _platformSpeed * Time.deltaTime);
+      //  _myTransform.position = new Vector2(pos, _myTransform.position.y);
+    }
+
+    void OnMouseDown()
+    {
+        _screenPoint = Camera.main.WorldToScreenPoint(_myTransform.position);
+
+
+
+        //  Vector2 offset = _myTransform.position - Camera.main.ScreenToWorldPoint(new Vector2(Input.mousePosition.x, Input.mousePosition.y));
+
+    }
+
+
+    void OnMouseDrag()
+    {
+       
+        Vector2 curScreenPoint = new Vector2(Input.mousePosition.x, _screenPoint.y);
+
+
+        Vector2 curPosition = Camera.main.ScreenToWorldPoint(curScreenPoint);
+
+        float leftBorder = Camera.main.transform.position.x - Camera.main.orthographicSize * Camera.main.aspect;
+        float rightBorder = Camera.main.transform.position.x + Camera.main.orthographicSize * Camera.main.aspect;
+
+        //if (curPosition.x > leftBorder + _platformBoundSize / 2 && curPosition.x < rightBorder - _platformBoundSize / 2)
+        //{
+        //    _myTransform.position = curPosition;
+        //}
+
+        
+        float posX = Mathf.Clamp(curPosition.x, leftBorder + _platformBoundSize / 2, rightBorder - _platformBoundSize / 2);
+        _myTransform.position = new Vector3(posX, curPosition.y);
+;
+
+
+    }
+}
